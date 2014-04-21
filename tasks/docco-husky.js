d6cab7ed947b20bd297c868a3fe3532acc79ca4a
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2014-04-21 20:33:32 +0300

    Refactored options.
    With new grunt tasks option.

--- tasks/docco-husky.js
+++ tasks/docco-husky.js
@@ -7,36 +7,13 @@
 module.exports = function(grunt) {
 
   var docco = require('docco-husky');
-  var _ = require('underscore');
 
   // ### TASKS
-  grunt.registerMultiTask('docco_husky', 'Docco-husky processor.', function() {
-    var options, tmp, cmd_options = [], args;
-
-    // Getting the configs.
-    tmp = grunt.config(['docco-husky', this.target, 'options']);
-    if (typeof tmp === 'object') {
-      grunt.verbose.writeln('Using "' + this.target + '" Docco-husky options.');
-      options = tmp;
-    } else {
-      grunt.verbose.writeln('Using master Docco-huskey options.');
-      args = grunt.config('docco_husky');
-      cmd_options.push('-name');
-      cmd_options.push(args.project_name);
-      _.each(args.files, function(path){
-        cmd_options.push(path);
-      });
-
-      options = cmd_options;
-    }
-
-    grunt.verbose.writeflags(options, 'Options');
-
-    var done = this.async();
-    var src = grunt.file.expandFiles(this.file.src);
-    docco.document(options, function(err, result, code){
-      grunt.log.writeln("Doccoed husky [" + src.join(", ") + "]; " + err ? err : "(No errors)" + "\n" + result + " " + code);
-      done();
-    });
+  grunt.registerMultiTask('docco-husky', 'Docco-husky processor.', function() {
+    var name = this.data.name ? this.data.name : grunt.config.get('pkg.name');
+    // Either set the destination in the files block, or (prefferred) in { options: output }
+    var options = this.filesSrc;
+    options.unshift('-name', '"' + name + '"');
+    docco.document(options, this.async());
   });
 };
